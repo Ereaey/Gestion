b270cecbeed9a0d444f891c06dfb7919d85eb5eb
Ereaey <anthony.arthus@gmail.com>
2016-10-12 08:38:24 +0200

    redesign

--- datanote.h
+++ datanote.h
@@ -50,11 +50,11 @@ class DataNote : public QObject
 
         QString domaineVide()
         {
-            return "<font color=\"#EB0000\">" + QString::number(m_d->getCurrentCommu()->domainesVides.size()) + "</font> - <i><b>" + QString::number((float)m_d->getCurrentCommu()->domainesVides.size() * 100.0 / (float)m_d->getCurrentCommu()->domainesKey.size()) + "% </b></i>";
+            return "<font color=\"#D49C0B\">" + QString::number(m_d->getCurrentCommu()->domainesVides.size()) + "</font> - <i><b>" + QString::number((float)m_d->getCurrentCommu()->domainesVides.size() * 100.0 / (float)m_d->getCurrentCommu()->domainesKey.size()) + "% </b></i>";
         }
         QString domaineFull()
         {
-            return "<font color=\"#D49C0B\">" + QString::number(m_d->getCurrentCommu()->domainesPlein.size()) + "</font>";
+            return "<font color=\"#097E00\">" + QString::number(m_d->getCurrentCommu()->domainesPlein.size()) + "</font>";
         }
         QString domaineAsservisseurs()
         {
